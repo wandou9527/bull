5b2aa22e0bd4d9441a6c2df5cc1f10139ffbbc69
liming <liming@ithro.com>
2020-11-23 11:14:24 +0800

    init project

---
+++ src/main/java/com/wandou/bull/BullApplication.java
@@ -0,0 +1,13 @@
+package com.wandou.bull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BullApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wandou/bull/BullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wandou.bull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
