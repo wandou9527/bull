31eb8e8a27688bd2a45e91206ee5fbdf7df4e3eb
liminglm.li <liminglm.li@qunar.com>
2022-06-12 21:44:26 +0800

    clear

--- src/test/java/com/wandou/bull/test/demo/StringDemo.java
+++ src/test/java/com/wandou/bull/test/demo/StringDemo.java
@@ -1,22 +1,10 @@
 package com.wandou.bull.test.demo;
 
-import org.junit.jupiter.api.Test;
-
 /**
  * @author liming
  * @date 2021/12/14 下午4:12
  */
 public class StringDemo {
 
-    private String string = "{\"boostBeginDelayHours\":0,\"boostEndDelayHours\":24,\"withDrawBeginDelayHours\":24,\"withDrawEndDelayHours\":360,\"withDrawEndTime\":\"\",\"completionBoostThreshold\":15,\"completionAmountThreshold\":10000,\"thresholdEnum\":\"COUNT\",\"hitBigKnifes\":[\"5\",\"23\",\"66\",\"90\"],\"totalKnifes\":null,\"maxAmountCent\":100000000,\"minAmountCent\":6000}";
 
-    @Test
-    public void m1() {
-
-    }
-
-    @Test
-    public void m2() {
-
-    }
 }
--- src/test/java/com/wandou/bull/test/demo/UrlEncodeDemo.java
+++ src/test/java/com/wandou/bull/test/demo/UrlEncodeDemo.java
@@ -3,9 +3,6 @@ package com.wandou.bull.test.demo;
 import org.junit.jupiter.api.Test;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 /**
  * @author liming
@@ -15,31 +12,12 @@ public class UrlEncodeDemo {
 
     @Test
     public void m1() throws UnsupportedEncodingException {
-        String json = "{\"depCityCode\":\"PEK\"}";
-        String encode = URLEncoder.encode(json, StandardCharsets.UTF_8.name());
-        System.out.println(encode);
 
-        encode = URLEncoder.encode(encode, StandardCharsets.UTF_8.name());
-        System.out.println(encode);
-
-        encode = URLEncoder.encode(encode, StandardCharsets.UTF_8.name());
-        System.out.println(encode);
 
     }
 
     @Test
     public void decode() throws UnsupportedEncodingException {
-        String json = "{\"depCityCode\":\"PEK\"}";
-        String encode = URLEncoder.encode(json, StandardCharsets.UTF_8.name());
-        System.out.println(encode);
 
-        encode = URLDecoder.decode(encode, StandardCharsets.UTF_8.name());
-        System.out.println(encode);
-
-        encode = URLDecoder.decode(encode, StandardCharsets.UTF_8.name());
-        System.out.println(encode);
-
-        encode = URLDecoder.decode(encode, StandardCharsets.UTF_8.name());
-        System.out.println(encode);
     }
 }
